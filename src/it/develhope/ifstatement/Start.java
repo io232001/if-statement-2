0720064dbc9a8f4bb235bd3d6ba91bf68382417a
iosbd <io232001@gmail.com>
2023-01-30 00:30:38 +0100

    Initial commit

---
+++ src/it/develhope/ifstatement/Start.java
@@ -0,0 +1,15 @@
+package it.develhope.ifstatement;
+public class Start {
+    public static void main(String args[])
+    {
+        String mySurname = "Mazilu";
+
+        if (mySurname.contains("ni")) {
+            System.out.println("Your surname contains the sequence of letters: ni");
+        } else {
+            System.out.println("Your surname doesn't contain the sequence of letters: ni");
+        }
+
+    }
+}
+
